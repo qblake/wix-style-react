7aea121e83c0380bad1268dd2bb3216b23be69ae
mykas <mykolass@wix.com>
2018-08-31 12:35:02 +0300

    e2e tests refactor

--- src/AddItem/AddItem.e2e.js
+++ src/AddItem/AddItem.e2e.js
@@ -35,101 +35,56 @@ describe('AddItem', () => {
   });
 
   describe(`'alignItems' prop`, () => {
-    eyes.it(`should render with value 'center' by default`, async () => {
-      await autoExampleDriver.setProps({});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
-
-    eyes.it(`should render with value 'left'`, async () => {
-      await autoExampleDriver.setProps({alignItems: 'left'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
-
-    eyes.it(`should render with value 'right'`, async () => {
-      await autoExampleDriver.setProps({alignItems: 'right'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
+    ['center', 'left', 'right'].map(alignItems =>
+      eyes.it(`should render with value ${alignItems}`, async () => {
+        await autoExampleDriver.setProps({
+          alignItems: alignItems === 'center' ? '' : alignItems
+        });
+        const driver = addItemTestkitFactory({
+          dataHook: storySettings.dataHook
+        });
+        await createDriverFactory(driver);
+      })
+    );
   });
 
   describe(`'theme' prop`, () => {
-    eyes.it(`should render with value 'dashes' by default`, async () => {
-      await autoExampleDriver.setProps({});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
-
-    eyes.it(`should render with value 'plain'`, async () => {
-      await autoExampleDriver.setProps({theme: 'plain'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
-
-    eyes.it(`should render with value 'filled'`, async () => {
-      await autoExampleDriver.setProps({theme: 'filled'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
-
-    eyes.it(`should render with value 'image'`, async () => {
-      await autoExampleDriver.setProps({theme: 'image'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
+    ['dashes', 'filled', 'plain', 'image'].map(theme =>
+      eyes.it(`should render with theme ${theme}`, async () => {
+        await autoExampleDriver.setProps({
+          disabled: true,
+          theme: theme === 'dashes' ? '' : theme
+        });
+        const driver = addItemTestkitFactory({
+          dataHook: storySettings.dataHook
+        });
+        await createDriverFactory(driver);
+      })
+    );
   });
 
   describe(`'size' prop`, () => {
-    eyes.it(`should render with large icon`, async () => {
-      await autoExampleDriver.setProps({size: 'large'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
-
-    eyes.it(`should render with mediumn icon`, async () => {
-      await autoExampleDriver.setProps({size: 'medium'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
-
-    eyes.it(`should render with small icon`, async () => {
-      await autoExampleDriver.setProps({size: 'small'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
-
-    eyes.it(`should render with tiny icon`, async () => {
-      await autoExampleDriver.setProps({size: 'tiny'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
+    ['large', 'medium', 'small', 'tiny'].map(size =>
+      eyes.it(`should render with ${size} icon`, async () => {
+        await autoExampleDriver.setProps({size: size === 'tiny' ? '' : size});
+        const driver = addItemTestkitFactory({
+          dataHook: storySettings.dataHook
+        });
+        await createDriverFactory(driver);
+      })
+    );
   });
 
   describe(`'disable' prop`, () => {
-    eyes.it(`should render with theme dashes`, async () => {
-      await autoExampleDriver.setProps({disabled: true});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
-
-    eyes.it(`should render with theme filled`, async () => {
-      await autoExampleDriver.setProps({disabled: true, theme: 'filled'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
-
-    eyes.it(`should render with theme plain`, async () => {
-      await autoExampleDriver.setProps({disabled: true, theme: 'plain'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
-
-    eyes.it(`should not apply any style with theme image`, async () => {
-      await autoExampleDriver.setProps({disable: true, theme: 'image'});
-      const driver = addItemTestkitFactory({dataHook: storySettings.dataHook});
-      await createDriverFactory(driver);
-    });
+    ['dashes', 'filled', 'plain', 'image'].map(theme =>
+      eyes.it(`should render with theme ${theme}`, async () => {
+        await autoExampleDriver.setProps({disabled: true, theme});
+        const driver = addItemTestkitFactory({
+          dataHook: storySettings.dataHook
+        });
+        await createDriverFactory(driver);
+      })
+    );
   });
 
   describe('Generic', () => {
